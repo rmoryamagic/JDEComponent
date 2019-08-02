2ef52074237e261865d116ce308127b7d9563e94
rmoryamagic <rmorya@magicsoftware.com>
2019-08-02 22:31:12 +0530

    change fields to property

--- src/main/java/io/elastic/jdee1/JdeCredentialsVerifier copy.java
+++ src/main/java/io/elastic/jdee1/JdeCredentialsVerifier copy.java
@@ -1,5 +1,5 @@
 package io.elastic.jdee1;
-import io.elastic.api.Message;
+
 import io.elastic.api.CredentialsVerifier;
 import io.elastic.api.InvalidCredentialsException;
 import javax.json.Json;
@@ -8,29 +8,21 @@ import javax.json.JsonObjectBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JdeCredentialsVerifierAction implements Module {
+public class JdeCredentialsVerifier implements CredentialsVerifier {
 
-  private static final Logger logger = LoggerFactory.getLogger(JdeCredentialsVerifierAction.class);
-
-  /**
-     * Executes the actions's logic by sending a request to the verify creden API and emitting response to the platform.
-     *
-     * @param parameters execution parameters
-     */
+  private static final Logger logger = LoggerFactory.getLogger(JdeCredentialsVerifier.class);
 
   @Override
-  public void execute(ExecutionParameters parameters) {
+  public void verify(JsonObject configuration) throws InvalidCredentialsException {
     logger.info("About to verify provided JDE E1 Credentials");
 
     Utils jdeinstance = new Utils();
-    final Message message = parameters.getMessage();
-    final JsonObject configuration = parameters.getConfiguration();
     final JsonObjectBuilder result = Json.createObjectBuilder();
     final JsonObjectBuilder snapshot = Json.createObjectBuilder();
-    final JsonObject body = message.getBody();//Json.createObjectBuilder();
+    final JsonObjectBuilder body = Json.createObjectBuilder();
     try {
       result.add("result",
-          jdeinstance.getTemplate_actionPerformed(configuration, snapshot.build(), body));
+          jdeinstance.getTemplate_actionPerformed(configuration, snapshot.build(), body.build()));
     } catch (Exception e) {
       throw new InvalidCredentialsException("Failed to connect to instance", e);
     } finally {
--- src/main/java/io/elastic/jdee1/JdeCredentialsVerifier.java
+++ src/main/java/io/elastic/jdee1/JdeCredentialsVerifier.java
@@ -1,35 +1,44 @@
 package io.elastic.jdee1;
-
+import io.elastic.api.Module;
 import io.elastic.api.CredentialsVerifier;
 import io.elastic.api.InvalidCredentialsException;
+import io.elastic.api.ExecutionParameters;
+import io.elastic.api.Message;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JdeCredentialsVerifier implements CredentialsVerifier {
+public class JdeCredentialsVerifier implements Module {
 
   private static final Logger logger = LoggerFactory.getLogger(JdeCredentialsVerifier.class);
 
+   /**
+     * Executes the actions's logic by sending a request to the verify creden API and emitting response to the platform.
+     *
+     * @param parameters execution parameters
+     */
   @Override
-  public void verify(JsonObject configuration) throws InvalidCredentialsException {
+  public void execute(ExecutionParameters parameters) {
     logger.info("About to verify provided JDE E1 Credentials");
 
     Utils jdeinstance = new Utils();
+    final Message message = parameters.getMessage();
+    final JsonObject configuration = parameters.getConfiguration();
     final JsonObjectBuilder result = Json.createObjectBuilder();
     final JsonObjectBuilder snapshot = Json.createObjectBuilder();
-    final JsonObjectBuilder body = Json.createObjectBuilder();
+    final JsonObject body = message.getBody();
     try {
       result.add("result",
-          jdeinstance.getTemplate_actionPerformed(configuration, snapshot.build(), body.build()));
+          jdeinstance.getTemplate_actionPerformed(configuration, snapshot.build(), body));
     } catch (Exception e) {
       throw new InvalidCredentialsException("Failed to connect to instance", e);
     } finally {
       if (result != null) {
         logger.info("Closing connection");
       } else {
-        logger.error("Failed to close connection");
+        logger.error("Failed to closed connection");
       }
     }
   }
