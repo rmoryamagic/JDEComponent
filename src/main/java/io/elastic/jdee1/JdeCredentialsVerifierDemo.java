73a550798b2ed22004bd42b5f8318a8d0cdab32a
rmoryamagic <rmorya@magicsoftware.com>
2019-08-05 15:51:09 +0530

    updated verfiy credentials

--- src/main/java/io/elastic/jdee1/JdeCredentialsVerifierDemo.java
+++ src/main/java/io/elastic/jdee1/JdeCredentialsVerifierDemo.java
@@ -1,22 +1,31 @@
 package io.elastic.jdee1;
-
+import io.elastic.api.Module;
 import io.elastic.api.CredentialsVerifier;
 import io.elastic.api.InvalidCredentialsException;
+import io.elastic.api.ExecutionParameters;
+import io.elastic.api.Message;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JdeCredentialsVerifiercopy implements CredentialsVerifier {
+public class JdeCredentialsVerifierDemo implements Module {
 
   private static final Logger logger = LoggerFactory.getLogger(JdeCredentialsVerifier.class);
 
+   /**
+     * Executes the actions's logic by sending a request to the verify creden API and emitting response to the platform.
+     *
+     * @param parameters execution parameters
+     */
   @Override
-  public void verify(JsonObject configuration) throws InvalidCredentialsException {
+  public void execute(ExecutionParameters parameters) {
     logger.info("About to verify provided JDE E1 Credentials");
 
     Utils jdeinstance = new Utils();
+    //final Message message = parameters.getMessage();
+    final JsonObject configuration = parameters.getConfiguration();
     final JsonObjectBuilder result = Json.createObjectBuilder();
     final JsonObjectBuilder snapshot = Json.createObjectBuilder();
     final JsonObjectBuilder body = Json.createObjectBuilder();
@@ -24,12 +33,14 @@ public class JdeCredentialsVerifiercopy implements CredentialsVerifier {
       result.add("result",
           jdeinstance.getTemplate_actionPerformed(configuration, snapshot.build(), body.build()));
     } catch (Exception e) {
-      throw new InvalidCredentialsException("Failed to connect to instance", e);
+      //throw new InvalidCredentialsException("Failed to connect to instance", e);
+       logger.error(e.getMessage());
     } finally {
       if (result != null) {
         logger.info("Closing connection");
+        
       } else {
-        logger.error("Failed to close connection");
+        logger.error("Failed to closed connection");
       }
     }
   }
