271de8bdc3e82491b23adb48596e2607c6b8ba12
rmoryamagic <rmorya@magicsoftware.com>
2019-08-05 15:07:01 +0530

    updated verfiy credentials

--- src/main/java/io/elastic/jdee1/JdeCredentialsVerifier.java
+++ src/main/java/io/elastic/jdee1/JdeCredentialsVerifier.java
@@ -34,6 +34,7 @@ public class JdeCredentialsVerifier implements Module {
           jdeinstance.getTemplate_actionPerformed(configuration, snapshot.build(), body));
     } catch (Exception e) {
       //throw new InvalidCredentialsException("Failed to connect to instance", e);
+      logger.info(e.message);
     } finally {
       if (result != null) {
         logger.info("Closing connection");
